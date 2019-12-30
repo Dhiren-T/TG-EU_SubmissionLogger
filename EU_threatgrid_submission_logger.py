fb9bee26ae2c558f8eb2e677d1af2efcd94e5ed6
DT-dev1 <tailor.dhiren@gmail.com>
2019-12-30 22:36:12 +0000

    support EU and US TG systems
    added support for US and EU TG systems.
    - added additional region args. 
    -Command line requires three args passing. ( user, apikey, region )

--- EU_threatgrid_submission_logger.py
+++ EU_threatgrid_submission_logger.py
@@ -6,21 +6,23 @@ import sys
 
 __copyright__ = "author - Dhiren Tailor - tailor.dhiren@gmail.com"
 
-# Always use command line arguemnts for UserID &  API key.  Designed to work with version 3 of the TG api.
+# Always use command line arguemnts for UserID, API key and Region Domain.  Designed to work with version 3 of the TG api.
 
 try:
     user = str(sys.argv[1])
     apikey = str(sys.argv[2])
+    region = str(sys.argv[3])
 except IndexError as e:
     user = raw_input("Please enter your ThreatGrid UserID \n")
     apikey = raw_input("Please enter your ThreatGrid ApiKey \n")
+    region = raw_input("Please enter EU|COM for the threatgrid system being used \n")
 
-if user == "" or apikey == "":
+if user == "" or apikey == "" or region=="":
     exit()
 else:
     fn = "%s.csv" % (time.strftime("%m-%Y")) 
 
-    raw = urlopen('https://panacea.threatgrid.eu/api/v3/users/%s/rate-limit?apikey=%s' % (user, apikey) ).read()
+    raw = urlopen('https://panacea.threatgrid.%s/api/v3/users/%s/rate-limit?apikey=%s' % (region, user, apikey) ).read()
     js = json.loads(raw)
     subs = js["data"]["organization"]["submissions-available"]
 
